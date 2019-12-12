10e0a6616f4a4dec1d4e77a1f019640e1727f66e
DeYeVe <dnwo44@naver.com>
2019-12-12 19:19:22 +0900

    객체 HashMap 추가

--- src/main/java/com/ninejo/helper/Service/DBControl.java
+++ src/main/java/com/ninejo/helper/Service/DBControl.java
@@ -7,6 +7,7 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DBControl {
     DBConnect db = new DBConnect();
@@ -78,59 +79,59 @@ public class DBControl {
         db.getResultmodify(sql);
     } // id, point 매개변수로 받아서 해당 id의 포인트 값 감소
 
-    public ArrayList<request> getRequestList () throws SQLException {
+    public ArrayList<HashMap<String, Object>> getRequestList () throws SQLException {
 
-        ArrayList<request> rlist = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> rlist = new ArrayList<>();
 
         String sql = String.format("select * from helper.request where is_completed = false");
 
         ResultSet rs = db.getResult(sql);
 
         while(rs.next()){
-            request r = new request();
-            r.req_num = rs.getInt("req_num");
-            r.requester_id = rs.getString("requester_id");
-            r.accepted_id = rs.getString("accepted_id");
-            r.reward = rs.getInt("reward");
-            r.title = rs.getString("title");
-            r.start_date = rs.getDate("start_date");
-            r.end_date = rs.getDate("end_date");
-            r.locate = rs.getString("locate");
-            r.contents = rs.getString("contents");
-            r.is_completed = rs.getBoolean("is_completed");
+            HashMap<String, Object> r = new HashMap<>();
+            r.put("req_num", rs.getInt("req_num"));
+            r.put("requester_id", rs.getString("requester_id"));
+            r.put("accepted_id", rs.getString("accepted_id"));
+            r.put("reward", rs.getInt("reward"));
+            r.put("title", rs.getString("title"));
+            r.put("start_date", rs.getDate("start_date"));
+            r.put("end_date", rs.getDate("end_date"));
+            r.put("locate", rs.getString("locate"));
+            r.put("contents", rs.getString("contents"));
+            r.put("is_completed", rs.getBoolean("is_completed"));
 
             rlist.add(r);
         }
         return rlist;
     } // request 클래스의 arraylist 반환.
 
-    public ArrayList<request> getSolvedRequestList (String id) throws SQLException {
+    public ArrayList<HashMap<String, Object>> getSolvedRequestList (String id) throws SQLException {
 
-        ArrayList<request> rlist = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> rlist = new ArrayList<>();
 
         String sql = String.format("select * from helper.request where is_completed = true & accepted_id = '%s'", id);
 
         ResultSet rs = db.getResult(sql);
 
         while(rs.next()){
-            request r = new request();
-            r.req_num = rs.getInt("req_num");
-            r.requester_id = rs.getString("requester_id");
-            r.accepted_id = rs.getString("accepted_id");
-            r.reward = rs.getInt("reward");
-            r.title = rs.getString("title");
-            r.start_date = rs.getDate("start_date");
-            r.end_date = rs.getDate("end_date");
-            r.locate = rs.getString("locate");
-            r.contents = rs.getString("contents");
-            r.is_completed = rs.getBoolean("is_completed");
+            HashMap<String, Object> r = new HashMap<>();
+            r.put("req_num", rs.getInt("req_num"));
+            r.put("requester_id", rs.getString("requester_id"));
+            r.put("accepted_id", rs.getString("accepted_id"));
+            r.put("reward", rs.getInt("reward"));
+            r.put("title", rs.getString("title"));
+            r.put("start_date", rs.getDate("start_date"));
+            r.put("end_date", rs.getDate("end_date"));
+            r.put("locate", rs.getString("locate"));
+            r.put("contents", rs.getString("contents"));
+            r.put("is_completed", rs.getBoolean("is_completed"));
 
             rlist.add(r);
         }
         return rlist;
     } // request 클래스의 arraylist 반환. 파라미터로 받은 id의 해결했던 의뢰들 리턴
 
-    public request getPostRequest (String id) throws SQLException {
+    public HashMap<String, Object> getPostRequest (String id) throws SQLException {
 
         int num = 0;
 
@@ -145,25 +146,24 @@ public class DBControl {
 
         rs = db.getResult(sql);
 
-        request r = new request();
-
+        HashMap<String, Object> r = new HashMap<>();
         while(rs.next()){
-            r.req_num = rs.getInt("req_num");
-            r.requester_id = rs.getString("requester_id");
-            r.accepted_id = rs.getString("accepted_id");
-            r.reward = rs.getInt("reward");
-            r.title = rs.getString("title");
-            r.start_date = rs.getDate("start_date");
-            r.end_date = rs.getDate("end_date");
-            r.locate = rs.getString("locate");
-            r.contents = rs.getString("contents");
-            r.is_completed = rs.getBoolean("is_completed");
+            r.put("req_num", rs.getInt("req_num"));
+            r.put("requester_id", rs.getString("requester_id"));
+            r.put("accepted_id", rs.getString("accepted_id"));
+            r.put("reward", rs.getInt("reward"));
+            r.put("title", rs.getString("title"));
+            r.put("start_date", rs.getDate("start_date"));
+            r.put("end_date", rs.getDate("end_date"));
+            r.put("locate", rs.getString("locate"));
+            r.put("contents", rs.getString("contents"));
+            r.put("is_completed", rs.getBoolean("is_completed"));
 
         }
         return r;
     } // id를 받아서 현재 요청중인 request 반환
 
-    public request getAcptRequest (String id) throws SQLException {
+    public HashMap<String, Object> getAcptRequest (String id) throws SQLException {
 
         int num = 0;
 
@@ -178,38 +178,37 @@ public class DBControl {
 
         rs = db.getResult(sql);
 
-        request r = new request();
-
+        HashMap<String, Object> r = new HashMap<>();
         while(rs.next()){
-            r.req_num = rs.getInt("req_num");
-            r.requester_id = rs.getString("requester_id");
-            r.accepted_id = rs.getString("accepted_id");
-            r.reward = rs.getInt("reward");
-            r.title = rs.getString("title");
-            r.start_date = rs.getDate("start_date");
-            r.end_date = rs.getDate("end_date");
-            r.locate = rs.getString("locate");
-            r.contents = rs.getString("contents");
-            r.is_completed = rs.getBoolean("is_completed");
+            r.put("req_num", rs.getInt("req_num"));
+            r.put("requester_id", rs.getString("requester_id"));
+            r.put("accepted_id", rs.getString("accepted_id"));
+            r.put("reward", rs.getInt("reward"));
+            r.put("title", rs.getString("title"));
+            r.put("start_date", rs.getDate("start_date"));
+            r.put("end_date", rs.getDate("end_date"));
+            r.put("locate", rs.getString("locate"));
+            r.put("contents", rs.getString("contents"));
+            r.put("is_completed", rs.getBoolean("is_completed"));
 
         }
         return r;
     } // id를 받아서 현재 해결중인 request 반환
 
-    public member getMemberInfo (String id) throws SQLException{
+    public HashMap<String, Object> getMemberInfo (String id) throws SQLException{
 
         String sql = String.format("select * from helper.member where id = '%s'", id);
         ResultSet rs = db.getResult(sql);
 
-        member m = new member();
+        HashMap<String, Object> m = new HashMap<>();
         while(rs.next()) {
-            m.id = rs.getString("id");
-            m.password = rs.getString("password");
-            m.phone_number = rs.getString("phone_number");
-            m.locate = rs.getString("locate");
-            m.point = rs.getInt("point");
-            m.req_request = rs.getInt("req_request");
-            m.acpt_request = rs.getInt("acpt_request");
+            m.put("id", rs.getString("id"));
+            m.put("password", rs.getString("password"));
+            m.put("phone_number", rs.getString("phone_number"));
+            m.put("locate", rs.getString("locate"));
+            m.put("point", rs.getInt("point"));
+            m.put("req_request", rs.getInt("req_request"));
+            m.put("acpt_request", rs.getInt("acpt_request"));
         }
         return m;
     }
@@ -347,27 +346,27 @@ public class DBControl {
 
     } // id를 매개변수로 받아서 등록한 의뢰가 있는지 검사. 있을경우 true, 없을경우 false
  }
-
- class request {
-    int req_num;
-    String requester_id;
-    String accepted_id;
-    int reward;
-    String title;
-    Date start_date;
-    Date end_date;
-    //Date 사용법 - String to Date : Date.valueOf("1111-11-11"); / Date to String : date.toString();
-    String locate;
-    String contents;
-    boolean is_completed;
-} //request 객체
-
-class member {
-    String id;
-    String password;
-    String phone_number;
-    String locate;
-    int point;
-    int req_request;
-    int acpt_request;
-} //member 객체
+//
+// class request {
+//    int req_num;
+//    String requester_id;
+//    String accepted_id;
+//    int reward;
+//    String title;
+//    Date start_date;
+//    Date end_date;
+//    //Date 사용법 - String to Date : Date.valueOf("1111-11-11"); / Date to String : date.toString();
+//    String locate;
+//    String contents;
+//    boolean is_completed;
+//} //request 객체
+//
+//class member {
+//    String id;
+//    String password;
+//    String phone_number;
+//    String locate;
+//    int point;
+//    int req_request;
+//    int acpt_request;
+//} //member 객체
